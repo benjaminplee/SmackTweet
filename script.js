53637f1e84df06adb1cd7d79bfb17935bbec7a58
Benjamin Lee <leebenjp@gmail.com>
2011-01-25 21:02:12 -0600

    Added hack for detecting offline mode even if browser doesn't recognize site is down

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 $(function() {
+	// THE "RIGHT" WAY TO DO IT
+	// var online = window.navigator.onLine
+
+	// HACK TO MAKE THINGS WORK EVEN IF BROWSER REPORTS ONLINE (e.g. chrome)
+	var online = false;
+	
+	$.ajax({
+		url: 'online.json',
+		cache: false,
+		dataType: 'jsonp',
+		jsonpCallback: 'onlineDetection',
+		success: function(data) {
+			online = data.online
+		}
+	})
+
 	$('.showLatestTweet').click(function() {
-		var handle = $(this).html();
 		
-		$.ajax({
-			url: 'http://search.twitter.com/search.json',
-			cache: false,
-			dataType: 'jsonp',
-			data: {
-				q: 'from:' + handle,
-				rpp: 1
-			},
-			success: function(data) {
-				alert(handle + ' said: \'' + data.results[0].text + '\'')
-			},
-			error : function() {
-				alert('OH NOEZ!')
-			}
-		})
+		alert(online)
+		
+		if(online) {	
+			var handle = $(this).html()
+		
+			$.ajax({
+				url: 'http://search.twitter.com/search.json',
+				cache: false,
+				dataType: 'jsonp',
+				data: {
+					q: 'from:' + handle,
+					rpp: 1
+				},
+				success: function(data) {
+					alert(handle + ' said: \'' + data.results[0].text + '\'')
+				},
+				error : function() {
+					alert('OH NOEZ!')
+				}
+			})
+		}
+		else {
+			alert('AINT GONNA DO IT!  WE IZ IN OFFLINE MODEZ!')
+		}
 		
 		return false
 	})
